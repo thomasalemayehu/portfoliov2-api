7cccce56714b64a78f44a57bdc924c269fc80f17
Thomas Alemayehu <thomas2alexmech@gmail.com>
2023-11-03 19:59:09 -0500

    Register, Login and Reset Password routes added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,118 @@
 import { Request, Response } from "express";
+import { Prisma } from "../util/Prisma.util";
+import { JsonWebToken } from "../util/Token.util";
+import { Bcrypt } from "../util/Bcyrpt.util";
 
 class AuthController {
   private static INSTANCE: AuthController | null = null;
 
   public static getInstance() {
-    if (this.INSTANCE) return this.INSTANCE;
+    if (this.INSTANCE == null || this.INSTANCE == undefined)
+      this.INSTANCE = new AuthController();
 
-    return new AuthController();
+    return this.INSTANCE;
   }
-  public registerUser(req: Request, res: Response): void {
-    res.status(200).json({ message: "Register User" });
+
+  public async registerUser(req: Request, res: Response): Promise<void> {
+    const {
+      email,
+      password,
+      name,
+    }: { email: string; password: string; name: string } = req.body;
+
+    if (!email) throw new Error("Email is required to register");
+    else if (!password) throw new Error("Password is required to register");
+    else if (!name) throw new Error("Name is required to register");
+
+    const hashedPassword = await Bcrypt.hashValue(password);
+    const newUser = await Prisma.getInstance().user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+      },
+    });
+
+    console.log(newUser);
+
+    const token = await JsonWebToken.getToken({ id: newUser.id, email, name });
+
+    res.status(201).json({ id: newUser.id, email, name, token });
   }
-  public loginUser(req: Request, res: Response): void {
-    res.status(200).json({ message: "Login User" });
+
+  public async loginUser(req: Request, res: Response): Promise<void> {
+    const { email, password }: { email: string; password: string } = req.body;
+
+    if (!email) throw new Error("Email is required to login");
+    else if (!password) throw new Error("Password is required to login");
+
+    const possibleUser = await Prisma.getInstance().user.findFirst({
+      where: { email: email },
+    });
+
+    if (!possibleUser) throw new Error("Invalid email and/or password");
+
+    const passwordMatch = await Bcrypt.compare(password, possibleUser.password);
+
+    if (!passwordMatch) throw new Error("Invalid email and/or password");
+
+    const token = await JsonWebToken.getToken({
+      id: possibleUser.id,
+      email: possibleUser.email,
+      name: possibleUser.name,
+    });
+
+    res.status(200).json({
+      id: possibleUser.id,
+      email: possibleUser.email,
+      name: possibleUser.name,
+      token,
+    });
   }
-  public resetPassword(req: Request, res: Response): void {
-    res.status(200).json({ message: "Reset Password" });
+
+  public async resetPassword(req: Request, res: Response): Promise<void> {
+    const {
+      email,
+      password,
+      newPassword,
+    }: { email: string; password: string; newPassword: string } = req.body;
+
+    if (!email) throw new Error("Email is required to change");
+    else if (!password) throw new Error("Password is required to change");
+    else if (!newPassword)
+      throw new Error("New Password is required to change");
+
+    const possibleUser = await Prisma.getInstance().user.findFirst({
+      where: { email: email },
+    });
+
+    if (!possibleUser) throw new Error("Invalid email and/or password");
+
+    const passwordMatch = await Bcrypt.compare(password, possibleUser.password);
+
+    if (!passwordMatch) throw new Error("Invalid email and/or password");
+
+    const hashedPassword = await Bcrypt.hashValue(newPassword);
+
+    await Prisma.getInstance().user.update({
+      where: { id: possibleUser.id },
+      data: { password: hashedPassword },
+    });
+
+    const token = await JsonWebToken.getToken({
+      id: possibleUser.id,
+      email: possibleUser.email,
+      name: possibleUser.name,
+    });
+
+    res.status(200).json({
+      id: possibleUser.id,
+      email: possibleUser.email,
+      name: possibleUser.name,
+      token,
+    });
   }
+
   public requestReset(req: Request, res: Response): void {
     res.status(200).json({ message: "Request Reset" });
   }
